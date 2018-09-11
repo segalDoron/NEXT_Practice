861c204066a12e4ff12ec03d61c9e167bc82e266
segalDoron <37837856+segalDoron@users.noreply.github.com>
2018-09-11 21:11:02 +0300

    Add files via upload

--- pages/login.js
+++ pages/login.js
@@ -1,43 +1,127 @@
 import React, { Fragment } from 'react'
 import styled from 'styled-components';
-
-const Input = styled.input.attrs({
-    type: props => props.type ? props.type : 'test',
-    placeholder: props => props.type == 'text' ? 'Enter User name' : 'Enter Password'
-})`
-  
-`;
-
-const Lable = styled.label`
-
-`
-
-
+import { connect } from 'react-redux'
+import { action } from '@storybook/react';
 
 const Container = styled.div`
   position: relative;
   border-radius: 5px;
   background-color: #f2f2f2;
-  padding: 20px 0 30px 0;
+  padding: ${props => props.conType ? '20px 0 0 0' : '0px'};
+  text-align: center;
+`
+const Header = styled.h2`
+text-align: center;
 `
 
-const LogInput = ({ lable, type }) => {
+const Row = styled.div`
+  width:100%
+  display: table;
+  clear: both;
+`
+
+const Col = styled.div`
+  float: left;
+  width: 100%;
+  display:${ props => props.colType ? "block" : "flex"};
+  margin: auto;
+`
+
+const Input = styled.input.attrs({
+    type: props => props.type ? props.type : 'text',
+    placeholder: props => props.type == 'text' ? 'Enter User name' : 'Enter Password'
+}) `
+width: 95%;
+padding: 12px;
+border: none;
+border-radius: 4px;
+margin: 5px 15px;
+opacity: 0.85;
+display: inline-block;
+font-size: 17px;
+line-height: 20px;
+text-decoration: none;
+`;
+
+const Btn = styled(Input).attrs({
+    type: "button"
+}) `
+  outline: none !important;
+  box-shadow: none;
+  background-color: ${ props => props.login ? "#4CAF50" : "#666"};
+  width:97%;
+  &:hover {
+    background-color:${ props => props.login ? "#45a049" : "#adadad"};
+  }
+`
+
+const Forgot = styled.a.attrs({
+    href: ''
+}) `
+pointer-events: none;
+cursor: default;
+`
+
+const LogInput = ({ type, actions }) => {
+    const action = type == 'text' ? actions.changeUserName : actions.changeUserPassword
     return (
         <Fragment>
-            <Lable>{lable}</Lable>
-            <Input type={type} />
+            <Input type={type} onChange={e => action(e.target.value)} />
         </Fragment>
     )
 }
 
-const LogIn = ({ header, user, loading, onClick, ...otherProps }) => (
-
-    <Fragment>
-        <Container>
-            <LogInput lable='name' type='text' />
-            <LogInput lable='password' type='password' />
+const Fields = ({ conType, rowType, colType, onClick, actions }) => {
+    return (
+        <Container conType>
+            <Row >
+                <Col colType>
+                    <LogInput type='text' actions={actions} />
+                    <LogInput type='password' actions={actions} />
+                    <Btn login value="login" onClick={() => { onClick.login(); actions.login() }}></Btn>
+                </Col>
+            </Row>
         </Container>
-    </Fragment>
-);
+    )
+}
 
-export default LogIn;
+const BtnContainer = ({ onClick }) => {
+    return (
+        <Container>
+            <Row >
+                <Col>
+                    <Btn value="Sign up" onClick={onClick.signUp}></Btn>
+                    <Btn value="Register" onClick={onClick.register}></Btn>
+                </Col>
+            </Row>
+            <Row >
+                <Col>
+                    <Forgot onClick={onClick.forget}>Forgot password?</Forgot>
+                </Col>
+            </Row >
+        </Container>
+    )
+}
+
+const LogIn = ({ header, loading, onClick, actions, ...otherProps
+
+
+}) => (
+
+        <Fragment>
+            <Header>{header}</Header>
+            <Fields conType='input' colType='inputRow' onClick={onClick} actions={actions} />
+            <BtnContainer onClick={onClick} />
+        </Fragment>
+    );
+
+export default connect(
+    state => ({
+        userName: state.userName,
+        loading: (
+            _.get(state, 'profile.loading') ||
+            _.get(state, 'customers.loading') ||
+            _.get(state, 'orders.loading')
+        )
+    })
+)(LogIn)
---
+++ pages/restaurantsList.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import styled from 'styled-components';
+
+const RestaurantsList = ({ addressList, header, onClick, ...otherProps }) => (
+
+    <Fragment>
+        <div></div>
+    </Fragment>
+);
+
+export default RestaurantsList;
