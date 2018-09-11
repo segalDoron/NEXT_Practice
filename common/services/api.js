19fe6ba502d60bf11dd5a70a34c2206c83ccf5ca
segalDoron <37837856+segalDoron@users.noreply.github.com>
2018-09-11 21:09:37 +0300

    Add files via upload

--- common/services/api.js
+++ common/services/api.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export const fetchUserProfile = userName => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -20,4 +22,16 @@ export const fetchCustomers = userName => {
       resolve([`customer1_of_${userName}`, `customer2_of_${userName}`])
     }, 1500)
   })
+}
+
+export const login = login => {
+  return axios.get('https://www.10bis.co.il/api/login?username=doron@10bis.co.il&password=Spot5564')
+    .then(function (response) {
+      return new Promise.resolve('ok')
+      console.log(response);
+    })
+    .catch(function (error) {
+      return new Promise.reject(error)
+      console.log(error);
+    });
 }
