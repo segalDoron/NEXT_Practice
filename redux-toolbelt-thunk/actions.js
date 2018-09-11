e9873574f07d213f322bf9a6f7b163d0f3db5f9a
segalDoron <37837856+segalDoron@users.noreply.github.com>
2018-09-11 21:11:47 +0300

    Add files via upload

--- redux-toolbelt-thunk/actions.js
+++ redux-toolbelt-thunk/actions.js
@@ -1,9 +1,11 @@
 import { makeActionCreator } from 'redux-toolbelt'
 import { makeThunkAsyncActionCreator } from 'redux-toolbelt-thunk'
+import axios from 'axios'
 
-import { fetchUserProfile, fetchCustomers, fetchOrders } from '../common/services/api'
+import { fetchUserProfile, fetchCustomers, fetchOrders,login } from '../common/services/api'
 
 export const changeUserName = makeActionCreator('CHANGE_USERNAME')
+export const changeUserPassword = makeActionCreator('CHANGE_PASSWORD')
 
 export const logout = makeActionCreator('LOGOUT')
 
@@ -11,11 +13,14 @@ export const loadProfile = makeThunkAsyncActionCreator('LOAD_PROFILE', fetchUser
 export const loadCustomers = makeThunkAsyncActionCreator('LOAD_CUSTOMERS', fetchCustomers)
 export const loadOrders = makeThunkAsyncActionCreator('LOAD_ORDERS', fetchOrders)
 
-export const login = makeThunkAsyncActionCreator('LOGIN', (e, {dispatch, getState}) => {
-  const { userName } = getState()
-  return Promise.all([
-    dispatch(loadProfile(userName)),
-    dispatch(loadCustomers(userName)),
-    dispatch(loadOrders(userName))
-  ])
-})
+export const login = makeThunkAsyncActionCreator('LOG_IN',login)
+
+// export const login = makeThunkAsyncActionCreator('LOGIN', (e, {dispatch, getState}) => {
+//   axios.get('https://www.10bis.co.il/api/login?username=doron@10bis.co.il&password=Spot5564')
+//   const { userName } = getState()
+//   return Promise.all([
+//     dispatch(loadProfile(userName)),
+//     dispatch(loadCustomers(userName)),
+//     dispatch(loadOrders(userName))
+//   ])
+// })
--- redux-toolbelt-thunk/reducers.js
+++ redux-toolbelt-thunk/reducers.js
@@ -1,13 +1,14 @@
 import { makeReducer, makeAsyncReducer, composeReducers } from 'redux-toolbelt'
 import { updateObjectProperties } from 'redux-toolbelt-immutable-helpers'
-import { loadCustomers, loadOrders, loadProfile, changeUserName, logout } from './actions'
+import { loadCustomers, loadOrders, loadProfile, changeUserName, logout ,changeUserPassword} from './actions'
 
 export default composeReducers(
   {
     profile: makeAsyncReducer(loadProfile),
     customers: makeAsyncReducer(loadCustomers),
     orders: makeAsyncReducer(loadOrders),
-    userName: makeReducer(changeUserName, { defaultState: 'user' })
+    userName: makeReducer(changeUserName, { defaultState: 'user' }),
+    password: makeReducer(changeUserPassword, { defaultState: '' })
   },
   makeReducer(logout, (state, { type, payload }) =>
     updateObjectProperties(state,
